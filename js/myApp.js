2a295446d4ebcd808ee2327cf8fbefc0d84d6588
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-17 12:44:09 -0400

    added gender customization ability

--- js/myApp.js
+++ js/myApp.js
@@ -1,7 +1,7 @@
 angular.module('myApp', [])
   .controller('myCtrl', function($scope) {
 
-    $scope.words = [{"name": "female name"},
+    $scope.words = [{"name": "name"},
                     {"job": "job title"},
                     {"task": "tedious task"},
                     {"task2": "dirty task"},
@@ -9,5 +9,21 @@ angular.module('myApp', [])
                     {"skill": "useless skill"},
                     {"adj": "adjective"},
                     {"celebrity": "obnoxious celebrity"},
-                    {"number": "huge number"}]
+                    {"number": "huge number"}];
+
+    $scope.gender = 'pick a gender';
+
+    $scope.male = function() {
+      $scope.gender = 'boy';
+      $scope.gender1 = 'he';
+      $scope.gender2 = 'his';
+      $scope.gender3 = 'him'
+    };
+
+    $scope.female = function() {
+      $scope.gender = 'girl';
+      $scope.gender1 = 'she';
+      $scope.gender2 = 'her';
+      $scope.gender3 = 'her';
+    };
 });
